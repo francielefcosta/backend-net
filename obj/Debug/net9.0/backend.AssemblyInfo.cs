29750ba01bf2ea9c67076a3e256330b996a9e945
Franciele <franciele.98.costa@gmail.com>
2025-05-21 13:33:06 -0300

    package

--- obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b949547677257636d9639508b99826c164441f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+171cb7709faca245417b3cd11f44468d060f0294")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
