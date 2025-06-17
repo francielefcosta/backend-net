25664a73ff569b9fb24212503bde977dae14f497
Franciele <franciele.98.costa@gmail.com>
2025-06-17 14:46:01 -0300

    atualização json

--- obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e39f2924ccd5a57efc4dc7c8f2f3c322549cc4dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be3db2c41f141ec88ca35a02227c5fbab719994")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
